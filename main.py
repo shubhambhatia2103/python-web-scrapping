9bacde440298b1713aa1af722fd6cf9a991e5f09
Shubham Bhatia <Shubhambhatia2103@gmail.com>
2024-04-18 18:16:29 +0530

    soup

--- main.py
+++ main.py
@@ -7,13 +7,5 @@ url = "https://books.toscrape.com/catalogue/page-1.html"
 page = requests.get(url)
 
 soup = BeautifulSoup(page.text, 'html.parser')
+
 print(soup.title.text)
-with open('index.html', 'r') as f:
-    contents = f.read()
-
-    soup = BeautifulSoup(contents, "html.parser")
-
-    for child in soup.descendants:
-
-        if child.name:
-            print(child.name)
